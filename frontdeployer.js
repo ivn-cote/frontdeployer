766b1f34d83d81af1550694207c3fbbc9168a60d
Vladislav Yakovlev <red.scorpix@gmail.com>
2013-12-09 20:24:29 +0400

    First commit.

---
+++ frontdeployer.js
@@ -0,0 +1,8 @@
+module.exports = {
+  Config: require('./lib/config'),
+  CssObfuscator: require('./lib/cssobfuscator'),
+  TemplateCompiler: require('./lib/templatecompiler'),
+  configGenerator: require('./lib/configgenerator'),
+  copier: require('./lib/copier'),
+  jsBuilder: require('./lib/jsbuilder')
+};
